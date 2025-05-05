629ad621709f62cde39499d04e0c093d45a3a44d
taketsugukoji <119645540+taketsugukoji@users.noreply.github.com>
2025-05-05 22:53:44 +0900

    linter系追加、一覧取得で更新日順にsortできるようにデータ型に日付系を追加

--- api/controller.py
+++ api/controller.py
@@ -1,27 +1,33 @@
 import json
 
 from sqlalchemy.orm import Session
-from model import create,update,delete,get_all,get_by_id
+from model import create, update, delete, get_all, get_by_id
 from schema.track_schema import TrackParams
 
 
-def create_track(db:Session,params:TrackParams):
+def create_track(db: Session, params: TrackParams):
     name = params.name
     # sqlite では配列での保存が対応されていない。pattern が検索等で使用される可能性はないため str で保存
     pattern_string = json.dumps(params.pattern.model_dump())
-    return create(db,name,pattern_string)
+    return create(db, name, pattern_string)
 
-def update_track(db:Session,id:int,params:TrackParams):
+
+def update_track(db: Session, id: int, params: TrackParams):
     name = params.name
     # sqlite では配列での保存が対応されていない。pattern が検索等で使用される可能性はないため str で保存
     pattern_string = json.dumps(params.pattern.model_dump())
-    return update(db,id,name,pattern_string)
+    return update(db, id, name, pattern_string)
 
-def delete_track(db:Session,id:int):
-    return delete(db,id)
 
-def get_all_tracks(db:Session):
-    return get_all(db)
+def delete_track(db: Session, id: int):
+    return delete(db, id)
 
-def get_track_by_id(db:Session,id:int):
-    return get_by_id(db,id)
+
+def get_all_tracks(db: Session):
+    tracks = get_all(db)
+    return [track.to_dict() for track in tracks]
+
+
+def get_track_by_id(db: Session, id: int):
+    track = get_by_id(db, id)
+    return track.to_dict() if track else None
--- api/routes.py
+++ api/routes.py
@@ -2,11 +2,18 @@ from fastapi import APIRouter
 from fastapi.params import Depends
 from sqlalchemy.orm import Session
 
-from controller import create_track, update_track, delete_track, get_all_tracks, get_track_by_id
+from controller import (
+    create_track,
+    update_track,
+    delete_track,
+    get_all_tracks,
+    get_track_by_id,
+)
 from model import SessionLocal
 from schema.track_schema import TrackParams
 
 router = APIRouter()  # インスタンス化が必要です
+
 
 def get_db():
     db = SessionLocal()
@@ -15,21 +22,26 @@ def get_db():
     finally:
         db.close()
 
+
 @router.post("/track")
-def create_endpoint(params:TrackParams, db: Session = Depends(get_db)):
+def create_endpoint(params: TrackParams, db: Session = Depends(get_db)):
     return create_track(db, params)
 
+
 @router.put("/track/{track_id}")
-def update_endpoint(track_id: int,params:TrackParams, db: Session = Depends(get_db)):
-    return update_track(db,track_id, params)
+def update_endpoint(track_id: int, params: TrackParams, db: Session = Depends(get_db)):
+    return update_track(db, track_id, params)
+
 
 @router.delete("/track/{track_id}")
 def delete_endpoint(track_id: int, db: Session = Depends(get_db)):
-    return delete_track(db,track_id)
+    return delete_track(db, track_id)
+
 
 @router.get("/track")
 def get_all_tracks_endpoint(db: Session = Depends(get_db)):
     return get_all_tracks(db)
+
 
 @router.get("/track/{track_id}")
 def get_track_by_id_endpoint(track_id: int, db: Session = Depends(get_db)):
